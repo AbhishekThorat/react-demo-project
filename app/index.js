2e0747cc9aca9b281bd360c153b9e341092dea74
AbhishekThorat <abhishek.thorat18@gmail.com>
2017-07-23 23:18:33 +0530

    Removed the globa scss file,

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { NotFound, Dashboard } from './components';
-import './styles/_index.scss';
+import './styles';
 
 ReactDOM.render(
   <Provider store={store}>
